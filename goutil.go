74b5bf05edcda1cd46e145d60af03f8e0e0ff71a
fegoa89 <fgonzalezaguilera@gmail.com>
2018-10-03 19:49:02 +0200

    added Capitalize and CapitalizeTitle function

--- goutil.go
+++ goutil.go
@@ -1,10 +1,46 @@
+// Package goutil is a collection of small reusable Go functions 
 package goutil
 
-// Reverse returns its argument string reversed rune-wise left to right.
-func Reverse(s string) string {
-	r := []rune(s)
-	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
-		r[i], r[j] = r[j], r[i]
+import(
+	"unicode"
+	"strings"
+)
+
+// Capitalizes the first letter of a string.
+func Capitalize(str string) string {
+    for i, v := range str {
+        return string(unicode.ToUpper(v)) + str[i+1:]
+    }
+    return ""
+}
+
+// Capitalizes string excluding words such as "and", "a", "with", "or".
+// You can provide an optional string slice with words that will be excluded too
+func CapitalizeTitle(input string, omitWords ...[]string) string {
+	title := strings.Fields(input)
+	lowercaseWords := []string{"and", "an", "a", "on", "with", "or"}
+
+	if len(omitWords) > 0 {
+		for _, word := range omitWords[0] {
+			lowercaseWords = append(lowercaseWords, word)
+		}
 	}
-	return string(r)
+
+	for index, word := range title {
+		if contains(lowercaseWords, word) {
+			title[index] = word
+		} else {
+			title[index] = strings.Title(word)
+		}
+	}
+	return strings.Join(title, " ")
+}
+
+func contains(s []string, e string) bool {
+    for _, a := range s {
+        if a == e {
+            return true
+        }
+    }
+    return false
 }
--- goutil_test.go
+++ goutil_test.go
@@ -2,18 +2,47 @@ package goutil
 
 import "testing"
 
-func TestReverse(t *testing.T) {
+func TestCapitalize(t *testing.T) {
 	cases := []struct {
 		in, want string
 	}{
-		{"Hello, world", "dlrow ,olleH"},
-		{"Hello, 世界", "界世 ,olleH"},
+		{"hallo", "Hallo"},
+		{"hola que tal", "Hola que tal"},
 		{"", ""},
 	}
+
 	for _, c := range cases {
-		got := Reverse(c.in)
+		got := Capitalize(c.in)
 		if got != c.want {
-			t.Errorf("Reverse(%q) == %q, want %q", c.in, got, c.want)
+			t.Errorf("Capitalize(%q) == %q, want %q", c.in, got, c.want)
+		}
+	}
+}
+
+func TestCapitalizeTitle(t *testing.T) {
+	cases := []struct {
+		in, want string
+	}{
+		{"foo and bar", "Foo and Bar"},
+	}
+
+	for _, c := range cases {
+		got := CapitalizeTitle(c.in)
+		if got != c.want {
+			t.Errorf("CapitalizeTitle(%q) == %q, want %q", c.in, got, c.want)
+		}
+	}
+
+	cases = []struct {
+		in, want string
+	}{
+		{"milk and honey", "milk and honey"},
+	}
+
+	for _, c := range cases {
+		got := CapitalizeTitle(c.in, []string{"honey", "milk"})
+		if got != c.want {
+			t.Errorf("CapitalizeTitle(%q) == %q, want %q", c.in, got, c.want)
 		}
 	}
 }
