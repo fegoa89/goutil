424231d6ba341b1f031d34aebedcf9ed08d5ca26
Fernando González Aguilera <fgonzalezaguilera@gmail.com>
2018-10-26 09:27:33 +0200

    added methods HoursBetween, MinutesBetween, SecondsBetween and MonthsBetween

--- timespan/timespan.go
+++ timespan/timespan.go
@@ -34,6 +34,26 @@ func (t TimeSpan) ValidDateRange() bool {
 }
 
 // Returns the quantity of days between two time objects
-func (t TimeSpan) DaysBetween() float64 {
-	return t.end.Sub(t.start).Hours() / 24
+func (t TimeSpan) DaysBetween() int {
+	return int(t.end.Sub(t.start).Hours() / 24)
+}
+
+// Returns the quantity of hours between two time objects
+func (t TimeSpan) HoursBetween() float64 {
+	return t.end.Sub(t.start).Hours()
+}
+
+// Returns the quantity of minutes between two time objects
+func (t TimeSpan) MinutesBetween() float64 {
+	return t.end.Sub(t.start).Minutes()
+}
+
+// Returns the quantity of seconds between two time objects
+func (t TimeSpan) SecondsBetween() float64 {
+	return t.end.Sub(t.start).Seconds()
+}
+
+// Returns the quantity of months between two time objects
+func (t TimeSpan) MonthsBetween() int {
+	return int(t.DaysBetween() / 30)
 }
--- timespan/timespan_test.go
+++ timespan/timespan_test.go
@@ -48,7 +48,7 @@ func TestValidDateRange(t *testing.T) {
 func TestDaysBetween(t *testing.T) {
 	cases := []struct {
 		start, end string
-		days       float64
+		days       int
 	}{
 		{"2018 01 11 00 00", "2018 10 11 00 00", 273},
 		{"2018 01 11 00 00", "2018 01 12 00 00", 1},
@@ -59,6 +59,75 @@ func TestDaysBetween(t *testing.T) {
 
 		if got != c.days {
 			t.Errorf("DaysBetween() returns %q, want %q", got, c.days)
+		}
+	}
+}
+
+func TestHoursBetween(t *testing.T) {
+	cases := []struct {
+		start, end string
+		hours      float64
+	}{
+		{"2018 10 11 00 00", "2018 10 11 03 00", 3},
+		{"2018 10 11 00 00", "2018 10 12 00 00", 24},
+	}
+
+	for _, c := range cases {
+		got := TimeSpanStruct(startTimeObject(c.start), endTimeObject(c.end)).HoursBetween()
+
+		if got != c.hours {
+			t.Errorf("HoursBetween() returns %q, want %q", got, c.hours)
+		}
+	}
+}
+
+func TestSecondsBetween(t *testing.T) {
+	cases := []struct {
+		start, end string
+		seconds    float64
+	}{
+		{"2018 10 11 00 00", "2018 10 11 00 03", 180},
+	}
+
+	for _, c := range cases {
+		got := TimeSpanStruct(startTimeObject(c.start), endTimeObject(c.end)).SecondsBetween()
+
+		if got != c.seconds {
+			t.Errorf("SecondsBetween() returns %q, want %q", got, c.seconds)
+		}
+	}
+}
+
+func TestMonthsBetween(t *testing.T) {
+	cases := []struct {
+		start, end string
+		seconds    int
+	}{
+		{"2018 10 01 00 00", "2018 11 01 00 00", 1},
+	}
+
+	for _, c := range cases {
+		got := TimeSpanStruct(startTimeObject(c.start), endTimeObject(c.end)).MonthsBetween()
+
+		if got != c.seconds {
+			t.Errorf("MonthsBetween() returns %q, want %q", got, c.seconds)
+		}
+	}
+}
+
+func TestMinutesBetween(t *testing.T) {
+	cases := []struct {
+		start, end string
+		minutes    float64
+	}{
+		{"2018 10 01 00 00", "2018 10 01 00 01", 1},
+	}
+
+	for _, c := range cases {
+		got := TimeSpanStruct(startTimeObject(c.start), endTimeObject(c.end)).MinutesBetween()
+
+		if got != c.minutes {
+			t.Errorf("MinutesBetween() returns %q, want %q", got, c.minutes)
 		}
 	}
 }
